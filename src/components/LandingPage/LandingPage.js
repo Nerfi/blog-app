0d3daed3fa7fba906ba9913cfec8fc9c2a40b785
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-04-24 23:08:27 +0200

    need to filter the results

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import Posts from  '../Posts/Posts';
 import './landingPage.css';
 
-const landinPage = () => <div className="app">  </div>;
+//make API call
+//filter post by most likes
 
 
-export default landinPage;
+const LandinPage = () => {
+   //filter result by post with most likes
+  const [blogs, setBlogs] =  useState([]);
+
+  async function fetchBlogPosts () {
+
+    const fetchPost = await fetch('/posts');
+    const response = await fetchPost.json();
+    setBlogs(response);
+
+  };
+  console.log(blogs)
+
+  useEffect(() => {
+    fetchBlogPosts();
+  },[]);
+
+  return(
+    <div>
+      <div className="app">
+      </div>
+      <p>Our Most Popular Posts </p>
+      {/*check out how to filter an array and render post with likes > 5*/}
+      {blogs.map(blog => {
+        return <Posts key={blog.id}/>
+      })}
+
+    </div>
+
+  );
+}
+
+export default LandinPage;
