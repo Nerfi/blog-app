eb9c447adc555dcfc115d201a5ed233a4be19a0e
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-08-03 14:02:06 +0200

    need to change firebase methods on user log in and sign up

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -7,37 +7,22 @@ import { useHistory } from 'react-router-dom';
 
 
 const Main = () => {
- // const history = useHistory();
-  //console.log(history, 'props from withRouter are here hopefully ')
-
 
     const {newData, setNewData} = useContext(UserContext);
-    // not sure why this works with Pascal and not with lowerSnakeCase
 
-    //const logOut = () => {
-
-      //const history = useHistory();
-
-      //setNewData({null}) // I have to clean out the last state , all the data now will be null after logOut
-
-      //return history.push("/");
-    //}
 
       const history = useHistory();
 
       const logOut = () => {
         //aqui vamos a borrar lo que hemos guardado en localStorage
-        //localStorage.removeItem('token')
-        //localStorage.removeItem('expirationTime')
-
-
-
+        localStorage.removeItem('token');
+        localStorage.removeItem('expirationTime');
 
       setNewData({token: null,useId: null })
       history.push("/")
     };
 
-   // const  history = useHistory();
+
 
   return(
        <div>
